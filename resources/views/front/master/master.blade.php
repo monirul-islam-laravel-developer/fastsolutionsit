6e9126bb77e8fe63980bcebcb840b9d3cdfc9a9f
monirul-islam <monirulislamlaraveldevoloper@gmail.com>
2024-11-04 01:28:26 +0600

    fff

--- resources/views/front/master/master.blade.php
+++ resources/views/front/master/master.blade.php
@@ -2,14 +2,7 @@
 <html lang="en">
 
 <head>
-    <meta name="language" content="en" />
-    <meta http-equiv="Content-Language" content="en" />
-    <meta name="robots" content="all">
-    <meta name="googlebot" content="all" />
-    <meta name="googlebot-news" content="all" />
-    <meta name="Developer" content="Fast Solutions It" />
-    <meta name="Developed By" content="Fast Solutions It" />
-    <meta name="author" content="Fast Solutions It" />
+    
     @yield('seo')
 
 
